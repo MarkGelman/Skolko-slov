8783aee051a9608035ff1b3db6abbc54403bb702
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-02 00:18:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skolko_slov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            /*Вводится строка.Посчитать количество слов в строке. Слова разделяются одним или несколькими пробелами.
+
+            Начальные данные: строка текста.
+            Вывод результата: 1 число - количество слов.
+
+            Полезные ссылки: Метод Split()*/
+        {
+            char[] charSeparators = new char[] { ' ' };
+            string[] s = Console.ReadLine().Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(s.Length);
+        }
+    }
+}
